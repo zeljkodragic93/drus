80b89f0f8167042ef459af28786aa2804cfba4fc
zeljkodragic93 <zeljkodragic93@gmail.com>
2017-01-22 23:32:45 +0100

    Dodat fajl

---
+++ DrusProject/Contracts/IObserverService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    [ServiceContract(SessionMode =SessionMode.Required, CallbackContract = typeof(IObseverServiceCallback))]
+    public interface IObserverService
+    {
+
+        [OperationContract(IsOneWay = true)]
+        void Subscribe(List<string> meterIDs);
+    }
+
+    
+    public interface IObseverServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void SendValue(double value, string meterId, MeterReadingType readingType);
+    }
+}
