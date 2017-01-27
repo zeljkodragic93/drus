a4e7cdf8723063e533d6463a055122b3e3b858a5
zeljkodragic93 <zeljkodragic93@gmail.com>
2017-01-27 16:40:03 +0100

    krajnji zadataka

--- DrusProject/Common/Database/ReportManager.cs
+++ DrusProject/Common/Database/ReportManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,56 @@ namespace Common.Database
                 {
                     ret.AppendFormat("Vrednost: {0}, Vreme ocitavanja: {1}.\n", meterRead.Value, meterRead.Timestamp);
                 }
+            }
+            return ret.ToString();
+        }
+
+        public string GenerateReport2(string meterID, int from)
+        {
+            StringBuilder ret = new StringBuilder();
+            foreach (var meterRead in _scadaEntites.MeterReads)
+            {
+                if (meterRead.Meter.Name == meterID && (from <= meterRead.Value) && meterRead.Type == (int)MeterReadingType.Humidity)
+                {
+                    ret.AppendFormat("Vrednost: {0}, Vreme ocitavanja: {1}.\n", meterRead.Value, meterRead.Timestamp);
+                }
+            }
+            return ret.ToString();
+        }
+
+        public string GenerateReport3(string meterID, int from)
+        {
+            StringBuilder ret = new StringBuilder();
+            foreach (var meterRead in _scadaEntites.MeterReads)
+            {
+                if (meterRead.Meter.Name == meterID && (from <= meterRead.Value) && meterRead.Type == (int)MeterReadingType.Temeprature)
+                {
+                    ret.AppendFormat("Vrednost: {0}, Vreme ocitavanja: {1}.\n", meterRead.Value, meterRead.Timestamp);
+                }
+            }
+            return ret.ToString();
+        }
+
+        public string GenerateReport4(string meterID)
+        {
+            StringBuilder ret = new StringBuilder();
+            foreach (var meterRead in _scadaEntites.MeterReads)
+            {
+                double sumT = 0;
+                double sumH = 0;
+                double noT = 0;
+                double noH = 0;
+                if (meterRead.Meter.Name == meterID && meterRead.Type == (int)MeterReadingType.Temeprature)
+                {
+                    sumT = sumT + meterRead.Value;
+                    noT++;
+                }
+                else
+                {
+                    sumH = sumH + meterRead.Value;
+                    noH++;
+                }
+                ret.AppendFormat("Prosecne vrednosti: \n temperature: {0}, \n Humidity: {1}.\n", sumT / noT, sumH / noH);
             }
             return ret.ToString();
         }
--- DrusProject/Reporting/Program.cs
+++ DrusProject/Reporting/Program.cs
@@ -25,9 +25,39 @@ namespace Reporting
                     MenuOption1(out meterID, out from, out  to);
                     Console.WriteLine(_reportManager.GenerateReport1(meterID, from, to));
                     File.WriteAllText("report.txt", _reportManager.GenerateReport1(meterID, from, to));
-                    Console.WriteLine("Uspesno kreirak izvestaj.");
+                    Console.WriteLine("Uspesno kreiran izvestaj.");
                     Console.ReadLine();
 
+                }
+                if (menuOption == "2")
+                {
+                    string meterID = string.Empty;
+                    int from = 0;
+                    MenuOption2(out meterID, out from);
+                    Console.WriteLine(_reportManager.GenerateReport2(meterID, from));
+                    File.WriteAllText("report1.txt", _reportManager.GenerateReport2(meterID, from));
+                    Console.WriteLine("Uspesno kreiran izvestaj.");
+                    Console.ReadLine();
+                }
+                if (menuOption == "3")
+                {
+                    string meterID = string.Empty;
+                    int from = 0;
+                    MenuOption3(out meterID, out from);
+                    Console.WriteLine(_reportManager.GenerateReport3(meterID, from));
+                    File.WriteAllText("report2.txt", _reportManager.GenerateReport3(meterID, from));
+                    Console.WriteLine("Uspesno kreiran izvestaj.");
+                    Console.ReadLine();
+                }
+                if (menuOption == "4")
+                {
+                    string meterID = string.Empty;
+                    int from = 0;
+                    MenuOption4(out meterID);
+                    Console.WriteLine(_reportManager.GenerateReport4(meterID));
+                    File.WriteAllText("report3.txt", _reportManager.GenerateReport4(meterID));
+                    Console.WriteLine("Uspesno kreiran izvestaj.");
+                    Console.ReadLine();
                 }
             }
             while (menuOption != "6");
@@ -40,10 +70,9 @@ namespace Reporting
             Console.Clear();
             Console.WriteLine("Izaberite opciju:");
             Console.WriteLine("1. Izvestaj za odredjeni merac.");
-            Console.WriteLine("2. Izvestaj za odredjeno merenje sa odredjenog meraca.");
-            Console.WriteLine("3. Izvestaj trenutaka kada su merenje bila van opsega za odredjeni merac.");
+            Console.WriteLine("2. Izvestaj za trenutke kada su mjeranja temperatura bila veca od odredjene vlaznosti.");
+            Console.WriteLine("3. Izvestaj za trenutaka kada su merenje vlaznosti bila veca od odredjene temperature.");
             Console.WriteLine("4. Izvestaj za prosek temperature/vlaznosti za odredjenu lokaciju.");
-            Console.WriteLine("5. Izvestaj trenutaaka kada su merenje bila van opsega za odredjenu lokaciju.");
             Console.WriteLine("6. Izlazak iz programa.");
             option = Console.ReadLine();
             return option;
@@ -57,6 +86,26 @@ namespace Reporting
             from = DateTime.Parse(Console.ReadLine());
             Console.WriteLine("Unesite do kada zelite:");
             to = DateTime.Parse(Console.ReadLine());
+        }
+        private static void MenuOption2(out string meterID, out int from)
+        {
+            Console.WriteLine("Unesite id meraca");
+            meterID = Console.ReadLine();
+            Console.WriteLine("Unesite vlaznost:");
+            from = int.Parse(Console.ReadLine());
+        }
+        private static void MenuOption3(out string meterID, out int from)
+        {
+            Console.WriteLine("Unesite id meraca");
+            meterID = Console.ReadLine();
+            Console.WriteLine("Unesite temperaturu:");
+            from = int.Parse(Console.ReadLine());
+        }
+
+        private static void MenuOption4(out string meterID)
+        {
+            Console.WriteLine("Unesite id meraca");
+            meterID = Console.ReadLine();
         }
     }
 }
